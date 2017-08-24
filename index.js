c77ff5c10cd554fa6b6ab12987dc3e2789e972c9
Wes <wes.coffay@gmail.com>
2017-08-23 21:07:05 -0400

    added button functionality to tray

--- index.js
+++ index.js
@@ -1,14 +1,25 @@
 const {app, Menu, Tray} = require('electron')
 
+// app.dock.hide()
+
 let tray = null
 app.on('ready', () => {
   tray = new Tray('tray_icon_black.png')
   const contextMenu = Menu.buildFromTemplate([
-    {label: 'Item1', type: 'radio'},
-    {label: 'Item2', type: 'radio'},
+    {label: 'clickButton', click() {console.log('clicked button')}},
+    {label: 'changeIcon', click(){
+      changeIcon(tray)
+    }},
     {label: 'Item3', type: 'radio', checked: true},
-    {label: 'Item4', type: 'radio'}
+    {label: 'Item4', type: 'radio'},
+    {role: 'quit'}
   ])
   tray.setToolTip('This is my application.')
   tray.setContextMenu(contextMenu)
 })
+
+function changeIcon(tray) {
+
+    tray.setImage('tray_icon_purple.png')
+
+}
