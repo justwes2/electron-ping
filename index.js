635dd73ff953f4c39a5c6267acd643045954a9ec
Wes <wes.coffay@gmail.com>
2017-08-26 23:46:53 -0400

    clean up code, elimiate uncalled fuctions, resources

--- index.js
+++ index.js
@@ -2,31 +2,19 @@ const {app, Menu, Tray} = require('electron')
 const notify = require('electron-main-notification')
 const ping = require('ping')
 
-// app.dock.hide()
+app.dock.hide()
 
 let tray = null
 let connected = null
 app.on('ready', () => {
   tray = new Tray('tray_icon_black.png')
   const contextMenu = Menu.buildFromTemplate([
-    {label: 'clickButton', click() {console.log('clicked button')}},
-    {label: 'changeIcon', click(){
-      changeIcon()
-    }},
-    {label: 'Ping Google', click() {
-      pingGoogle()
-    }},
-    {label: 'Item4', type: 'radio'},
     {role: 'quit'}
   ])
   tray.setToolTip('This is my application.')
   tray.setContextMenu(contextMenu)
   setInterval(pingGoogle, 1000)
 })
-
-function changeIcon() {
-  tray.setImage('tray_icon_purple.png')
-}
 
 function showResults(alive) {
   if (alive && !connected) {
